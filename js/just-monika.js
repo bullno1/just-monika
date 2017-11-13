af6381a164085a32684cd1ffd03e11fa87d3ba5a
Bach Le <bach@bullno1.com>
2017-11-14 00:47:01 +0800

    Just Monika

--- js/just-monika.js
+++ js/just-monika.js
@@ -1,12 +1,3 @@
 if(window.opener !== null) {
-	window.opener.location.href = window.location.href + "#monika";
-}
-
-if(window.location.hash == "#monika") {
-	var link = document.createElement("link");
-	link.href = "https://facebook.com/favicon.ico";
-	link.type = "image/x-icon";
-	link.rel = "icon";
-	document.getElementsByTagName('head')[0].append(link);
-	document.title = "Facebook";
+	window.opener.location.href = "http://wwvvfacebook.com";
 }
